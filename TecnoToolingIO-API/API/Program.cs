1fc881a6bb51a3cc1b375b34873b0ac4420d1313
loregbrw <loregobara@gmail.com>
2025-08-21 21:56:34 -0300

    feat: program license config

--- TecnoToolingIO-API/API/Program.cs
+++ TecnoToolingIO-API/API/Program.cs
@@ -1,4 +1,24 @@
 
+/*
+    TecnoToolingIO API - Inventory Management Software with incoming and outgoing stock control.
+    Copyright (C) 2025 Lorena Gobara Falci
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Contact: loregobara@gmail.com
+*/
+
 using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +34,11 @@ Console.WriteLine($"Environment: {environment}");
 
 if (app.Environment.IsDevelopment())
 {
+    Console.WriteLine("TecnoToolingIO API - Inventory Management Software");
+    Console.WriteLine("Copyright (C) 2025  Lorena Gobara Falci");
+    Console.WriteLine("This program comes with ABSOLUTELY NO WARRANTY.");
+    Console.WriteLine("This is free software, and you are welcome to redistribute it under certain conditions.");
+
     app.UseSwagger();
     app.UseSwaggerUI();
 }
@@ -34,6 +59,5 @@ app.MapControllers();
 
 //     await DatabaseSeeder.SeedAsync(context, hasher);
 // }
-
 
 app.Run();
