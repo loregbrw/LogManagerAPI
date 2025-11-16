bd64fe85622e40cf2213307e1b4b98ee64044b93
loregbrw <loregobara@gmail.com>
2025-11-15 22:19:37 -0300

    feat: attributes

---
+++ LogManagerAPI/Application/Attributes/AppRequiredAttribute.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.Attributes;
+
+using System.ComponentModel.DataAnnotations;
+using Application.Exceptions;
+
+public class AppRequiredAttribute : RequiredAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is null || (value is string str && string.IsNullOrWhiteSpace(str)))
+        {
+            var fieldName = validationContext.DisplayName ?? validationContext.MemberName;
+                throw new BadRequestException("FieldRequired", fieldName!);
+        }
+
+        return ValidationResult.Success;
+    }
+}
