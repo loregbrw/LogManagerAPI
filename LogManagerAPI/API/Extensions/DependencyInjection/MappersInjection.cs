6c9a1f0deec2252c098bf2f0417b5612eeba5c57
loregbrw <loregobara@gmail.com>
2025-11-24 20:14:23 -0300

    fix: unit of measurement injection

--- LogManagerAPI/API/Extensions/DependencyInjection/MappersInjection.cs
+++ LogManagerAPI/API/Extensions/DependencyInjection/MappersInjection.cs
@@ -10,7 +10,7 @@ public static class MappersInjection
     public static IServiceCollection AddMappers(this IServiceCollection services)
     {
         services.AddScoped<IStockItemMapper, StockItemMapper>();
-        services.AddScoped<IUnitOfMeasurementMapper>();
+        services.AddScoped<IUnitOfMeasurementMapper, UnitOfMeasurementMapper>();
         services.AddScoped<IUserMapper, UserMapper>();
         services.AddScoped<IUserDepartmentMapper, UserDepartmentMapper>();
 
